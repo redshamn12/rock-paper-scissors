3d575fda34ac6892e27d12f1d07dc20d167a9ee9
redshamn12 <9pvsatria@gmail.com>
2022-04-24 13:52:18 +0700

    Create computerPlay() for random action

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+function computerPlay(){
+    let pcstate = ["Rock", "Paper", "Scissors"]
+    return ranstate = pcstate[Math.floor(Math.random() * pcstate.length)];
+}
+
+console.log(computerPlay());
