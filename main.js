6b76c3797034be1a7e4aad896d9d497a952d8266
redshamn12 <9pvsatria@gmail.com>
2022-04-24 16:33:21 +0700

    Adding game() and actionPlay()

--- main.js
+++ main.js
@@ -4,44 +4,43 @@ function computerPlay(){
 }
 
 function actionPlay(playerSelection, computerSelection){
-    switch (playerSelection === "Paper"){
-        case computerSelection === "Rock":
-            return message = "You won! Paper beats Rock";
-            break;
-        case computerSelection === "Scissors":
-            return message = "You lose! Scissors beats Paper";
-            break;
-        case computerSelection === "Paper":
-            return message = "Tie! Paper vs Paper"
-            break;
+    
+    if (playerSelection === "" && computerSelection != null){
+        return result = "Select action first!";
+    } else if (playerSelection.length <= 2 && computerSelection != null){
+        return result = "Select action first!";
     }
 
-    switch (playerSelection === "Rock"){
-        case computerSelection === "Rock":
-            return message = "Tie! Rock vs Rock";
-            break;
-        case computerSelection === "Scissors":
-            return message = "You won! Rock beats Scissors";
-            break;
-        case computerSelection === "Paper":
-            return message = "You lose! Paper beats Rock"
-            break;
-    }
+    if(playerSelection === "Rock" && computerSelection === "Rock" || playerSelection === "Paper" && computerSelection === "Paper" || playerSelection === "Scissors" && computerSelection === "Scissors"){
+        return result = "Tie! Player: "+playerSelection+" vs Computer: "+computerSelection;
+    } 
 
-    switch (playerSelection === "Scissors"){
-        case computerSelection === "Rock":
-            return message = "You lose! Rock beats Scissors";
-            break;
-        case computerSelection === "Scissors":
-            return message = "Tie! Scissors vs Scissors";
-            break;
-        case computerSelection === "Paper":
-            return message = "You won! Scissors beats Paper"
-            break;
+    if(playerSelection === "Rock" && computerSelection === "Paper"){
+        return result = "You lose! Computer: "+computerSelection+" beats Player: "+playerSelection;
+    } else if(playerSelection === "Paper" && computerSelection === "Rock"){
+        return result = "You won! Player: "+playerSelection+" beats Computer: "+computerSelection;
+    } else if(playerSelection === "Paper" && computerSelection === "Scissors"){
+        return result = "You lose! Computer: "+computerSelection+" beats Player: "+playerSelection;
+    } else if(playerSelection === "Scissors" && computerSelection === "Paper"){
+        return result = "You won! Player: "+playerSelection+" beats Computer: "+computerSelection;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors"){
+        return result = "You won! Player: "+playerSelection+" beats Computer: "+computerSelection;
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+        return result = "You lose! Computer: "+computerSelection+" beats Player: "+playerSelection;
     }
 }
 
-const playerSelection = "Paper";
-const computerSelection = computerPlay();
+function game(){
+    for (let i = 1; i <= 5; i++) {
+        let actionChoose = prompt("Select your action! Choose between Rock, Paper, Scissors");
+        const section1 = actionChoose.charAt(0).toUpperCase();
+        const section2 = actionChoose.slice(1).toLowerCase();
+        const playerSelection = section1+section2;
+        const computerSelection = computerPlay();
+        matchplay = actionPlay(playerSelection,computerSelection);
+        matchround = "Match - "+i;
+        console.log(matchround, matchplay);
+     }
+}
 
-console.log(actionPlay(playerSelection,computerSelection));
+game();
