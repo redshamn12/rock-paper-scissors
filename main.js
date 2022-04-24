d4c9924212caeaffddacd15b3b59e70a4ed66555
redshamn12 <9pvsatria@gmail.com>
2022-04-24 14:47:03 +0700

    Adding switch to actionPlay()

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
 function computerPlay(){
-    let pcstate = ["Rock", "Paper", "Scissors"]
+    const pcstate = ["Rock", "Paper", "Scissors"]
     return ranstate = pcstate[Math.floor(Math.random() * pcstate.length)];
 }
 
-console.log(computerPlay());
+function actionPlay(playerSelection, computerSelection){
+    switch (playerSelection === "Paper"){
+        case computerSelection === "Rock":
+            return message = "You won! Paper beats Rock";
+            break;
+        case computerSelection === "Scissors":
+            return message = "You lose! Scissors beats Paper";
+            break;
+        case computerSelection === "Paper":
+            return message = "Tie! Paper vs Paper"
+            break;
+    }
+
+    switch (playerSelection === "Rock"){
+        case computerSelection === "Rock":
+            return message = "Tie! Rock vs Rock";
+            break;
+        case computerSelection === "Scissors":
+            return message = "You won! Rock beats Scissors";
+            break;
+        case computerSelection === "Paper":
+            return message = "You lose! Paper beats Rock"
+            break;
+    }
+
+    switch (playerSelection === "Scissors"){
+        case computerSelection === "Rock":
+            return message = "You lose! Rock beats Scissors";
+            break;
+        case computerSelection === "Scissors":
+            return message = "Tie! Scissors vs Scissors";
+            break;
+        case computerSelection === "Paper":
+            return message = "You won! Scissors beats Paper"
+            break;
+    }
+}
+
+const playerSelection = "Paper";
+const computerSelection = computerPlay();
+
+console.log(actionPlay(playerSelection,computerSelection));
