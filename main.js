7c4534d0be09a926c5664c0080921110a21307d0
redshamn12 <9pvsatria@gmail.com>
2022-06-03 17:36:09 +0700

    Completing javascript

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 let playerSelection;
 let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let matchRound = 0;
+
 let buttons = document.querySelectorAll(".cbutton");
+let rbuttons = document.querySelector(".rbutton");
+let players = document.querySelector(".player-score");
+let computers = document.querySelector(".computer-score");
+let finals = document.querySelector(".final-res");
+let playAgain = document.querySelector(".play-again");
+let hideAction = document.querySelector("#action-button");
 
 /*Adding a randomly action for computer with bellow function*/
 function computerPlay(){
@@ -14,56 +24,75 @@ buttons.forEach((xbutton) => {
         const res = xbutton.value;
         playerSelection = res.toLowerCase();
         actionPlay(playerSelection, computerSelection);
+        players.innerHTML = playerScore;
+        computers.innerHTML = computerScore;
     })
+})
+
+rbuttons.addEventListener("click", () =>{
+    resetGame();
 })
 
 /* Checking the playerSelection vs computerSelection */
 function actionPlay(playerSelection, computerSelection){
     computerSelection = computerPlay().toLowerCase();
     playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection,computerSelection   );
+    matchRound = ++matchRound;
+
     /*first one is checking empty strings, second one is uncompleted strings bellow than 2 characters */
     if (playerSelection === "" && computerSelection != null){
        resultx = "Select action first!";
-       console.log(resultx);
     } else if (playerSelection.length <= 2 && computerSelection != null){
         resultx = "Select action first!";
-        console.log(resultx);
     }
 
     /* checking tie result */
     if(playerSelection === "rock" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection === "scissors"){
         cresultx = "Tie! Player: "+playerSelection+" vs Computer: "+computerSelection;
-        console.log(resultx);
     }
-     
+
     if(playerSelection === "rock" && computerSelection === "paper"){ /*check player rock vs computer paper */
         resultx = "You lose! Computer: "+computerSelection+" beats Player: "+playerSelection;
-        console.log(resultx);
+        computerScore = ++computerScore;
     } else if(playerSelection === "paper" && computerSelection === "rock"){ /*check player paper vs computer rock */
         resultx = "You won! Player: "+playerSelection+" beats Computer: "+computerSelection;
-        console.log(resultx);
+        playerScore = ++playerScore;
     } else if(playerSelection === "paper" && computerSelection === "scissors"){ /*check player paper vs computer scissors*/
         resultx = "You lose! Computer: "+computerSelection+" beats Player: "+playerSelection;
-        console.log(resultx);
+        computerScore = ++computerScore;
     } else if(playerSelection === "scissors" && computerSelection === "paper"){ /*check player scissors vs computer paper */
         resultx = "You won! Player: "+playerSelection+" beats Computer: "+computerSelection;
-        console.log(resultx);
+        playerScore = ++playerScore;
     } else if (playerSelection === "rock" && computerSelection === "scissors"){ /*check player rock vs computer scissors */
         resultx = "You won! Player: "+playerSelection+" beats Computer: "+computerSelection;
-        console.log(resultx);
+        playerScore = ++playerScore;
     } else if (playerSelection === "scissors" && computerSelection === "rock"){ /*check player scissors vs computer rock */
         resultx = "You lose! Computer: "+computerSelection+" beats Player: "+playerSelection;
-        console.log(resultx);
+        computerScore = ++computerScore;
     }
+    game();
     } 
     
 
 /* Main function for the game, round is 5 times */
 function game(){
-    for (let i = 1; i <= 5; i++) {
-        matchPlay = actionPlay(playerSelection,computerSelection); /* calling funtion actionPlay */
-        matchRound = "Match - "+i; /* count the match */
-        console.log(matchRound, matchPlay); /* print the matchround and matchplay */
+    if (computerScore >= playerScore && computerScore === 5) {
+        finals.innerHTML = "Match end, the winner is Computer";
+        playAgain.style = "display: block";
+        hideAction.style = "display: none";
+
+     }else if (playerScore >= computerScore && playerScore === 5){
+        finals.innerHTML = "Match end, the winner is You";
+        playAgain.style = "display: block";
+        hideAction.style = "display: none";
      }
+}
+
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    matchRound = 0;
+    hideAction.style = "display: show";
+    playAgain.style = "display: none";
+    finals.innerHTML = "";
 }
